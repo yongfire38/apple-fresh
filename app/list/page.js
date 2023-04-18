38e46533423a02017195c185250699c7d6f908dd
yongfire38 <yongfire38@gmail.com>
2023-04-18 17:45:35 +0900

    Fix: 상품 개별 수량 변경 구현

--- app/list/page.js
+++ app/list/page.js
@@ -1,20 +1,42 @@
+"use client";
+import { useState } from "react";
+
 export default function List() {
-    
-  let products = ['Tomatoes','Pasta','Coconut']
-  
+  let products = ["Tomatoes", "Pasta", "Coconut"];
+  let [count, setCount] = useState([0, 0, 0]);
+
   return (
-      <div>
-        <h4>Products</h4>
-          {
-            products.map((product, i)=> {
-              return (
-                <div className="food" key={i}>
-                  <img src={`/food${i}.png`} alt={product} className="food-img"/>
-                  <h4>{product} $40</h4>
-                </div>
-              )
-            })
-          }
-      </div>
-    )
-  }
+    <div>
+      <h4>Products</h4>
+      {products.map((product, i) => {
+        return (
+          <div className="food" key={i}>
+            <img src={`/food${i}.png`} alt={product} className="food-img" />
+            <h4>{product} $40</h4>
+            <button
+              onClick={() => {
+                let copy = [...count];
+                copy[i]--;
+
+                setCount(copy);
+              }}
+            >
+              -
+            </button>
+            <span>{count[i]}</span>
+            <button
+              onClick={() => {
+                let copy = [...count];
+                copy[i]++;
+
+                setCount(copy);
+              }}
+            >
+              +
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
